5483ed75d495cdbbc2224f4f3529d66f9194070d
mavis acheampong <amavis32@yahoo.com>
2020-08-25 15:51:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
 import Navigation from "./components/navigation/Navigation";
 import Page404 from "./components/page404/Page404";
+import Jeopardy from "./components/jeopardy/Jeopardy";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         />
         <Route path="/clock" component={Clock} />
         <Route path="/contact" component={Contact} />
+        <Route path="/jeopardy" component={Jeopardy} />
         <Route component={Page404} />
       </Switch>
       {/* <Welcome name="May" />
